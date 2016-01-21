3fb19927561209ba9a9cafc47fa3ee71c618929f
anil-ninja <kumar.anil8892@gmail.com>
2016-01-21 20:34:41 +0530

    cem

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,8 @@
 <?php
-	$db_handle = mysqli_connect("localhost","root","redhat111111","bluenethack");
+	$db_handle = mysqli_connect("localhost","root","redhat@11111p","bluenethack_v0");
 
 //Check connection
 	if (mysqli_connect_errno()) {
 	  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-//wget -c https://d21.usercdn.com/d/4ql6qnxzt52fvxijvdxmtaqpdnn6xj4ojyb6ej7qpmpb3obte6utcpuy/Katti%20Batti%202015%20Hindi%20www.9xmovies.in%20720p%20DVDRip.mkv
 ?>
