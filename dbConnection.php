ffab3da5faf994fd9794fde4a5d41a2179abda23
anil-ninja <kumar.anil8892@gmail.com>
2016-01-27 13:49:16 +0530

    cem

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,8 @@
 <?php
-	$db_handle = mysqli_connect("localhost","root","redhat111111","bluenethack");
+	$db_handle = mysqli_connect("localhost","root","redhat@11111p","bluenethack_v0");
 
 //Check connection
 	if (mysqli_connect_errno()) {
 	  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-//wget -c https://d10.usercdn.com/d/rqlq7c6zt52fvxijilxyjbqoat4bktb5wfa7wnecp767gyoudva6py7d/Poojai%202014%20Dual%20Audio%20720p%20Hindi%20Tamil%209xmovies.mkv
 ?>
